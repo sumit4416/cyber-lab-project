4aa6e9d14a20fb256dd3e49ac878d7f6cdca8ff4
Sumit solanki <sumitsolanki750910@gmail.com>
2026-03-31 15:40:06 +0530

    Use environment variable for server port
    Update server to use environment variable for port

--- server/app.py
+++ server/app.py
@@ -150,5 +150,7 @@ def get_blocked():
 
 
 # ---------- MAIN ----------
+ import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
